a5a9b308467c23844744cc7ffedb1ab44d3485a6
panchiraakan <48082836+panchiraakan@users.noreply.github.com>
2023-06-11 12:40:22 +0400

    Add files via upload

--- pokeapi.py
+++ pokeapi.py
@@ -1,19 +1,33 @@
 import requests
+from flask import request
+
 
 def retrieve_pokemons():
-
-    url = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+    offset = request.args.get('offset') or 0
+    print(offset)
+    url = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=" + offset
 
     r = requests.get(url, timeout=5)
     r = r.json()
+    data = r["results"]
+    for idx, pokemon in enumerate(r["results"]):
+        url = "https://pokeapi.co/api/v2/pokemon/" + pokemon["name"]
+        r = requests.get(url, timeout=5)
+        r = r.json()
+        #print(r)
+        image = r['sprites']['front_default']
+        data[idx]["image_url"] = image
+        id=r['id']
+        url = "https://pokeapi.co/api/v2/pokemon-species/" + str(id)
+        r = requests.get(url, timeout=5)
+        r = r.json()
+        print(r)
+        data[idx]["image_url"] = image
+        # name = r['name']
+        # types = r['types'][0]['type']['name']
+    return data
 
-    id    = r['id']
-    name  = r['name']
-    #image = r['sprites']['front_default']
-    #types = r['types'][0]['type']['name']
-
-    print(id)
-    print(name)
-    print(r)
-
-    return{'id':id}
+    #return {"id": id, "name": name, "image_url": image}
+    #print(id, name, image)
+    #res=jsonify({"id": id, "name": name, "image_url": image})
+    #return make_response(jsonify(res), 200)
